ddf187e79409aafa2585dc76b0f78f1beef36058
Akash Regmi <akashregmi82@gmail.com>
2025-02-03 21:21:49 +0545

    Done with adding Add Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,46 @@ import { useState } from "react";
 import Button from "./component/Button";
 
 function App() {
-  const [todo,setTodo]=useState([]);
-  const[inputValue,setInputValue]=useState("");
+  const [todo, setTodo] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-  const handelChange=(e)=>{
-    
+  const handelChange = (e) => {
     setInputValue(e.target.value);
-  }
-  const addToDo=(e)=>{
+  };
+  const addToDo = (e) => {
     e.preventDefault();
     console.log(e.target.value);
-    if (inputValue.trim() === "") return; 
-    setTodo([...todo,inputValue]);
+    if (inputValue.trim() === "") return;
+    setTodo([...todo, inputValue]);
     setInputValue("");
-  }
+  };
 
-  return <>
-  
-  <h1>CRUD APP</h1>
-  <input type="text"  placeholder="Learn Cooking"  onChange={handelChange}/>
-  <Button name="Add" style={{backgroundColor:"green"}} onClick={addToDo}/>
-  <ul>
-  {todo.map((todoq,index)=>{
-    return(<li key={index}>{todoq}
-      
-      
-      </li>)
-    
-  })
-
-  }</ul>
-  
-  
-  </>;
+  return (
+    <>
+      <h1>CRUD APP</h1>
+      <div
+        
+        style={{ display: "flex", alignItems: "center", gap: "10px" }}
+      >
+        <input
+          type="text"
+          value={inputValue}
+          placeholder="Learn Cooking"
+          onChange={handelChange}
+        />
+        <Button
+          name="Add"
+          style={{ backgroundColor: "green" }}
+          onClick={addToDo}
+        />
+      </div>
+      <ul>
+        {todo.map((todoq, index) => {
+          return <li key={index}>{todoq}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
